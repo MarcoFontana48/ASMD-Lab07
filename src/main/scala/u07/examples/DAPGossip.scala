49ca6cc1472eb06e1a236c33fb26b641a13313ed
Marco Fontana <marco.fontana98@outlook.it>
2025-04-15 09:02:23 +0200

    done (only missing README)

--- src/main/scala/u07/examples/DAPGossip.scala
+++ src/main/scala/u07/examples/DAPGossip.scala
@@ -16,16 +16,15 @@ object DAPGossip:
   export u07.modelling.CTMCSimulation.*
 
   val gossipRules = DAP[Place](
-    Rule(MSet(A,A), m => 1000,  MSet(A),  MSet()),   // a|a --1000--> a
-    Rule(MSet(A),   m => 1,     MSet(A),  MSet(A)),       // a --1--> a|^a
+    Rule(MSet(A,A), m => 1000,  MSet(A),  MSet(A)),
+    Rule(MSet(A),   m => 1,     MSet(A),  MSet(A)),
   )
   val gossipCTMC = DAP.toCTMC[ID, Place](gossipRules)
   val net = Grids.createRectangularGrid(5, 5)
-  // an `a` initial on top LEFT
   val state = State[ID, Place](MSet(Token((0, 0), A)), MSet(), net)
 
 @main def mainDAPGossip =
   import DAPGossip.*
   gossipCTMC.newSimulationTrace(state,new Random).take(50).toList.foreach: step =>
-    println(step._1) // print time
-    println(DAPGrid.simpleGridStateToString[Place](step._2, A)) // print state, i.e., A's
+    println(step._1)
+    println(DAPGrid.simpleGridStateToString[Place](step._2, A))
